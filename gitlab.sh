d2df60802f9182a7dbcb112100799b964a8a4dbd
Douglas M Cotrim P Melo <douglas.cotrim@arivo.com>
2022-08-03 19:39:47 -0300

    feat: improve setup script

--- gitlab.sh
+++ gitlab.sh
@@ -1,22 +1,60 @@
 #!/usr/bin/env bash
 
-# TODO: try to fetch these params somewhere if possible
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+GRAY="\033[1;37m"
+ENDCOLOR="\033[0m"
 
-echo "Please, provide your gitlab access token:"
-read -sr GITLAB_ACCESS_TOKEN
+GITLAB_ACCESS_TOKEN="$1"
+if [ -z "$GITLAB_ACCESS_TOKEN" ]; then
+    msg="${BOLD}Please, provide your gitlab access token${NORMAL}"
+    msg="${msg} ${GRAY}[output is hidden]: ${ENDCOLOR}"
+    echo -en "$msg"
+    read -sr GITLAB_ACCESS_TOKEN
+    echo
+fi
 
-echo "Now, we need the project ID (you can get it through the gitlab api):"
-read -sr GITLAB_PROJECT_ID
+GITLAB_PROJECT_ID="$2"
+if [ -z "$GITLAB_PROJECT_ID" ];then
+    echo -en "${BOLD}Now, we need the project ID:${NORMAL} "
+    read -r GITLAB_PROJECT_ID
+fi
 
-echo "What is the git branch you're fetching from (defaults to 'main')?"
-read -r GIT_BRANCH
+GIT_BRANCH="$3"
+if [ -z "$GIT_BRANCH" ]; then
+    echo -en "${BOLD}What is the git branch?${NORMAL} ${GRAY}[default is 'main']:${ENDCOLOR} "
+    read -r GIT_BRANCH
+fi
+
+INSTALL_PATH="$4"
+if [ -z "$INSTALL_PATH" ]; then
+    msg="${BOLD}What is the install.sh path?${NORMAL}"
+    msg="${msg} ${GRAY}[default is 'install/install.sh']${ENDCOLOR}"
+    echo -en "$msg"
+    read -r INSTALL_PATH
+fi
 
 SLASH="%2F"
+INSTALL_PATH=$(echo "$INSTALL_PATH" | sed -e 's/\//%2F/')
 
 GITLAB_PROJECT_URL="https://gitlab.com/api/v4/projects/${GITLAB_PROJECT_ID}"
-INSTALL_PATH="repository/files/install${SLASH}install.sh"
-INSTALL_URL="${GITLAB_PROJECT_URL}/${INSTALL_PATH}/raw?ref=${GIT_BRANCH}"
+FULL_INSTALL_PATH="repository/files/${INSTALL_PATH:-install${SLASH}install.sh}"
+INSTALL_URL="${GITLAB_PROJECT_URL}/${FULL_INSTALL_PATH}/raw?ref=${GIT_BRANCH:-main}"
 
-echo "Fetching..."
+echo "Fetching $INSTALL_URL."
 
-curl -s -H "PRIVATE-TOKEN: ${GITLAB_ACCESS_TOKEN}" "${INSTALL_URL}" | bash && echo "...done!"
+HEADERS="PRIVATE-TOKEN: $GITLAB_ACCESS_TOKEN"
+STATUS_CODE=$(curl -s -H "$HEADERS" "$INSTALL_URL" -o /tmp/gl-install.sh -w "%{http_code}")
+if [ "$STATUS_CODE" != "200" ]; then
+    echo "Error requesting install script."
+    cat /tmp/gl-install.sh
+    EXIT_CODE=1
+else
+    bash /tmp/gl-install.sh "$GITLAB_ACCESS_TOKEN" && echo "...done!"
+    EXIT_CODE=0
+fi
+
+rm /tmp/gl-install.sh
+exit $EXIT_CODE
